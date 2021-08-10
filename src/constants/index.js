f9d6a010b3fae3b2f915def7cde46970809625b2
Kate Kostina <katekostina@yahoo.com>
2021-08-10 15:23:06 +0300

    Heroku server link added

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,10 @@ const BASE_MOVIES_URL = 'https://api.nomoreparties.co';
 // const BASE_MAIN_SERVER_URL =
 //   'https://api.diploma.katekostina.students.nomoredomains.monster';
 
-// for dev server only:
-const BASE_MAIN_SERVER_URL = 'http://localhost:3001';
+// local server
+// const BASE_MAIN_SERVER_URL = 'http://localhost:3001';
+
+// heroku server
+const BASE_MAIN_SERVER_URL = 'https://movies-explorer-api-server.herokuapp.com';
 
 export { BASE_MOVIES_URL, BASE_MAIN_SERVER_URL };
